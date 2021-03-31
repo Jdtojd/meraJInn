9dc243c00bb27a647aa2a3760a53eaecb0aa7b3d
Jaydeep Singh <jaydeepsingh1919@gmail.com>
2021-04-01 00:47:57 +0530

    first commit

---
+++ login/test.php
@@ -0,0 +1,18 @@
+<?php
+$number = rand(10,100);
+ 
+ 
+ 
+ 
+echo "<span style=\"background-image:url(\"images/yellowcircle.png\")><?php echo $number; ?> </span>";
+echo "<span style=\"background-image:url(\"images/yellowcircle.png\")><?php echo $number; ?> </span>"; 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+echo "<BUTTON TYPE=\'button\"  onClick=\"history.go(0)\" >Refresh</BUTTON>";
+?>
